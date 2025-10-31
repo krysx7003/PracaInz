52bf5050d4d0dbae3d3aa5991f3165f915e086e6
krysx7003 <krysx7003@gmail.com>
2025-10-31 15:22:58 +0100

    Added depreciated/ to store old scripts

---
+++ depriciated/find_char.py
@@ -0,0 +1,141 @@
+"""(module) find_chars."""
+
+import re
+
+
+def clean_text(text: str):
+    """Finds and replaces wrongly mapped chars.
+
+    Args:
+        text (str): Text read from pdf file using font JunicodeWL-Regular.
+
+    Returns:
+        str: Text in which invalid characters were replaced by closest known match. If char is invalid and there is no match it will be printed out.
+    """
+    pattern = re.compile(r"(\\u[0-9a-fA-F]{4})|[\ue000-\uf8ff]|([^\x00-\x7FążźćńółęśĄŻŹĆŃÓŁĘŚ])")
+
+    glyphs_to_code = {
+        "\u0105": "ą",
+        "\u0119": "ę",
+        "\u01f3": "n",
+        "\ueeca": "f",
+        "\u0133": "j",
+        "\u013a": "ĺ",
+        "\u01c9": "lj",
+        "\u01f1": "Ę",
+        "\u01f2": "Dz",
+        "\u24b2": "(w)",
+        "\ufb00": "ff",
+        "\ufb01": "fi",
+        "\ufb02": "fl",
+        "\u0c17": "j",
+        # Polish big
+        "\u0c1a": "Ą",
+        "\u0c1b": "Ć",
+        "\u0c1c": "Ę",
+        "\u0c1d": "Ł",
+        "\u0c1e": "Ń",
+        "\u0c1f": "Ś",
+        "\u0c20": "Ź",
+        "\u0c21": "Ż",
+        "\u1f00": "ἀ",
+        # Special
+        "\u2013": "-",
+        "\u2014": "-",
+        "\u201e": '"',
+        "\u201d": '"',
+        "\u2019": "'",
+        "\u2025": "..",
+        "\u2026": "...",
+        "\u203c": "!!",
+        # Numeric
+        "\u2070": "0",
+        "\u2071": "1",
+        "\u2072": "2",
+        "\u2073": "3",
+        "\u2074": "4",
+        "\u2075": "5",
+        "\u2076": "6",
+        "\u2077": "7",
+        "\u2078": "8",
+        "\u2079": "9",
+        "\uf730": "0",
+        "\uf731": "1",
+        "\uf732": "2",
+        "\uf733": "3",
+        "\uf734": "4",
+        "\uf735": "5",
+        "\uf736": "6",
+        "\uf737": "7",
+        "\uf738": "8",
+        "\uf739": "9",
+        # Cyrylic
+        "\u041a": "K",
+        "\u041f": "П",
+        "\u0420": "P",
+        "\u0430": "a",
+        "\u0432": "B",
+        "\u0438": "и",
+        "\u043c": "M",
+        "\u043b": "л",
+        "\u043e": "o",
+        "\u043d": "H",
+        "\u0441": "c",
+        "\u0442": "T",
+        "\u0447": "ч",
+        # Greek
+        "\u03c2": "ς",
+        "\u03ac": "ά",
+        "\u03c3": "σ",
+        "\u03c9": "ω",
+        "\u03b3": "γ",
+        "\u03bd": "v",
+        "\u03b9": "ι",
+        # Escape chars
+        "\uf761": "",
+        "\uf763": "",
+        "\uf764": "",
+        "\uf765": "",
+        "\uf769": "",
+        "\uf76a": "",
+        "\uf76b": "",
+        "\uf76d": "",
+        "\uf774": "",
+        "\uf777": "",
+        "\uf77a": "",
+        "\xe1": "",
+        "\xb3": "",
+        "\xab": "",
+        "\xe2": "",
+        "\xf4": "",
+        "\xea": "",
+        "\xe4": "",
+        "\xbb": "",
+        "\xe8": "",
+        "\xf6": "",
+        "\xb2": "",
+        "\xe9": "",
+        "\xfc": "",
+        "\xb9": "",
+        "\xe7": "",
+        "\xe0": "",
+    }
+
+    unique_chars: set[str] = set()
+    for glyph, unicode_char in glyphs_to_code.items():
+        text = text.replace(glyph, unicode_char)
+
+    matches: list[str] = []
+    for match in pattern.finditer(text):
+        groups = match.groups()
+        for m in groups:
+            if m is not None:
+                matches.append(m)
+
+    for char in matches:
+        unique_chars.add(f"{char}: {char.encode('unicode_escape').decode('ascii')}")
+
+    for char in unique_chars:
+        print(char)
+
+    return text
---
+++ depriciated/old_load_data.py
@@ -0,0 +1,172 @@
+import json
+import os
+import shutil
+
+import requests
+
+from dataset.book_json import Book
+from utils.path import is_valid
+
+AMOUNT = 15
+TIMEOUT_MAX = 1
+AUTHOR = "adam-mickiewicz"
+RAW_DIR = "./dataset/raw"
+CACHE_DIR = "./dataset/.cache"
+
+
+def open_json(path: str) -> list[Book]:
+    """Read and deserialize json file.
+
+    Args:
+        path (str): Valid json file
+
+    Return:
+        list[Book]: List of deserialized json objects
+    """
+    with open(path, encoding="utf-8") as file:
+        data = json.load(file)
+        return [Book(**book_data) for book_data in data]
+
+
+def get_book_list(use_cache: bool) -> None | list[Book]:
+    """Makes a request using wolnelektur api listing all books for AUTHOR.
+
+    Args:
+        use_cache (bool): If true CACHE_DIR will be searched for {AUTHOR}.json if it exists it will be returned else request will be made and result will be saved.
+
+    Return:
+        None: If request failed
+        list[Book]: List of deserialized json objects
+    """
+    url = f"https://wolnelektury.pl/api/authors/{AUTHOR}/books/"
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    path = os.path.join(CACHE_DIR, f"{AUTHOR}.json")
+    if is_valid(path) and use_cache:
+        print(f"File {AUTHOR}.json already exists, skipping url call")
+        return open_json(path)
+
+    try:
+        print(f"Getting: {url}")
+        response = requests.get(url, timeout=TIMEOUT_MAX)
+
+        if response.status_code != 200:
+            print("Error: ", response.status_code)
+            return None
+
+        books_data = response.json()
+        if use_cache:
+            print(f"Sqving file {AUTHOR}.json")
+
+            data = response.json()
+            with open(path, "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=2, ensure_ascii=False)
+
+        return [Book(**book) for book in books_data]
+
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
+
+
+def extract_name(book: Book) -> str:
+    """Splits url of the book to get properly formatted name.
+
+    Args:
+        book (Book): Deserialized result of api request
+
+    Returns:
+        str: Formatted name.
+    """
+    url_frags = book.url.split("/")
+    name = ""
+    if url_frags:
+        name = url_frags[-2]
+
+    return name
+
+
+def get_pdf_url(name: str) -> None | str:
+    """Makes a request using wolnelektur api.
+
+    Args:
+        name (str): Name of the book to be searched for.
+
+    Result:
+        None: If request failed.
+        str: Url of {name}.pdf
+    """
+    url = f"https://wolnelektury.pl/api/books/{name}"
+    try:
+        print(f"Getting: {url}")
+        response = requests.get(url, timeout=TIMEOUT_MAX)
+
+        if response.status_code != 200:
+            print("Error: ", response.status_code)
+            return None
+
+        if (data := response.json()) and isinstance(data, dict):
+            return data.get("pdf")
+
+        return None
+
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
+
+
+def scrape(pdf_url: str, name: str):
+    """Get pdf document from wolnelektur and save it to RAW_DIR.
+
+    Args:
+        pdf_url (str): Url pointing to documnet url.
+        name (str): Name of the book.
+    """
+    print("Downloading url: ", pdf_url)
+    try:
+        print(f"Getting: {pdf_url}")
+        response = requests.get(pdf_url, timeout=TIMEOUT_MAX)
+
+        if response.status_code != 200:
+            print("Error: ", response.status_code)
+
+        path = os.path.join(RAW_DIR, f"{name}.pdf")
+        with open(path, "wb") as file:
+            file.write(response.content)
+
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+
+
+def load_data(use_cache: bool = True, purge: bool = False):
+    """Load first AMOUNT books from AUTHOR aviable on wolnelektury.pl. Save pdf files in RAW_DIR.
+
+    Args:
+        use_cache (bool): If true script will first try to locate file in CACHE_DIR. By default True.
+        purge (bool): If true script will clean CACHE_DIR and RAW_DIR on every run.
+    """
+    if purge:
+        print("Purging...")
+        shutil.rmtree(RAW_DIR)
+        os.makedirs(RAW_DIR)
+        shutil.rmtree(CACHE_DIR)
+        os.makedirs(CACHE_DIR)
+
+    os.makedirs(RAW_DIR, exist_ok=True)
+
+    json_arr = get_book_list(use_cache)
+
+    if json_arr:
+        for i in range(0, AMOUNT):
+            name = extract_name(json_arr[i])
+
+            path = os.path.join(RAW_DIR, f"{name}.pdf")
+            if not use_cache or not is_valid(path, "pdf"):
+                url = get_pdf_url(name)
+                if url:
+                    scrape(url, name)
+                else:
+                    print("Error: Failed to load url")
+            else:
+                print(f"File {name}.pdf already exists, skipping download")
+    else:
+        print(f"Error: The author {AUTHOR} has no books")
